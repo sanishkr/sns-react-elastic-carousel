dd6cbd89def90495e167191aa9db5f4100d53eba
Sanish <skumar1@tila.com>
2019-11-22 18:31:29 +0530

    :bento: assets

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -9,10 +9,10 @@ export const imports = {
     import(/* webpackPrefetch: true, webpackChunkName: "mdx-focus-on-select" */ 'mdx/focusOnSelect.mdx'),
   'mdx/goTo.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "mdx-go-to" */ 'mdx/goTo.mdx'),
-  'mdx/index.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "mdx-index" */ 'mdx/index.mdx'),
   'mdx/initialFirstItem.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "mdx-initial-first-item" */ 'mdx/initialFirstItem.mdx'),
+  'mdx/index.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "mdx-index" */ 'mdx/index.mdx'),
   'mdx/isRTL.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "mdx-is-rtl" */ 'mdx/isRTL.mdx'),
   'mdx/itemPadding.mdx': () =>
